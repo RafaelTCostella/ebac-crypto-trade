1df3f0928d413c97fa1742a71416a4c739d9066f
Rafael Costella <rafael.costella@zrp.com.br>
2022-10-09 17:46:52 -0300

    Adicionada seed para inserir topclients

--- seed.js
+++ seed.js
@@ -1,1 +1,54 @@
+require('dotenv').config();
+
+// pode remover o top clients por qualquer outro nome de model
+// que você preferir
+const { Corretora, connect, Usuario, TopClients } = require('./models');
+const { CNPJ, RESERVA_MINIMA } = require('./constants');
+const { mongoose } = require('mongoose');
+
 // popula base de dados
+(async () => {
+  await connect();
+
+  await Corretora.findOneAndUpdate({
+    cnpj: CNPJ,
+  }, {
+    caixa: RESERVA_MINIMA,
+  }, {
+    upsert: true,
+  });
+
+  // insere usuários
+  const usuarioUm = await Usuario.findOneAndUpdate({
+    cpf: '90925683094',
+    email: 'juninho@ebac.com.br',
+  }, {
+    nome: 'Juninho da Silva Santos',
+    senha: await bcrypt.hash('ebac1234', 10),
+  }, {
+    upsert: true,
+  });
+
+  const usuarioDois = await Usuario.findOneAndUpdate({
+    cpf: '70200659022',
+    email: 'mariazinha@ebac.com.br',
+  }, {
+    nome: 'Mariazinha da silva',
+    senha: await bcrypt.hash('ebac1234', 10),
+  }, {
+    upsert: true,
+  });
+
+  const ontem = (new Date()).setDate((new Date()).getDate() - 1);
+  const anteOntem = (new Date()).setDate((new Date()).getDate() - 2);
+  const tresDiasAtras = (new Date()).setDate((new Date()).getDate() - 3);
+
+  // insere ranking
+  await TopClients.insertMany([
+    { dia: ontem, gainers: [ { usuario: usuarioUm, variacao: 200 } ], loosers: [ { usuario: usuarioDois, variacao: -1300 }] },
+    { dia: anteOntem, gainers: [ { usuario: usuarioDois, variacao: 2000 } ], loosers: [ { usuario: usuarioUm, variacao: -100 }] },
+    { dia: tresDiasAtras, gainers: [ { usuario: usuarioDois, variacao: 100 } ], loosers: [ { usuario: usuarioUm, variacao: -2000 }] },
+  ])
+
+  await mongoose.disconnect();
+})();
